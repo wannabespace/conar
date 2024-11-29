6c8cab9c1e06c09463bf179f2b8ac05e25c0342e
Valerii Strilets <valerii.strilets@gmail.com>
2024-11-30 01:01:26 +0200

    refactor

--- ui/app/app/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-
-import dynamic from 'next/dynamic'
-
-const App = dynamic(() => import('../../../app/src/renderer/src/App'), { ssr: false })
-
-export default function Page() {
-  return <App />
-}
