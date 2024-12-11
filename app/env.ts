158cf681c299d8461cdd4a6e79a3cbeec6fb86ea
Valerii Strilets <valerii.strilets@gmail.com>
2024-12-11 23:34:15 +0200

    update envs

--- app/env.ts
+++ app/env.ts
@@ -3,11 +3,11 @@ import { z } from 'zod'
 
 export const env = createEnv({
   shared: {
-    NEXT_PUBLIC_API_URL: z.string().min(1),
+    NEXT_PUBLIC_URL: z.string().min(1),
     NEXT_PUBLIC_IS_DESKTOP: z.enum(['true', 'false']).transform(t => t === 'true'),
   },
   experimental__runtimeEnv: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    NEXT_PUBLIC_URL: process.env.NEXT_PUBLIC_URL,
     NEXT_PUBLIC_IS_DESKTOP: process.env.NEXT_PUBLIC_IS_DESKTOP,
   },
 })
--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -2,5 +2,5 @@ import { createAuthClient } from 'better-auth/client'
 import { env } from '~/env'
 
 export const authClient = createAuthClient({
-  baseURL: env.NEXT_PUBLIC_API_URL,
+  baseURL: env.NEXT_PUBLIC_URL,
 })
