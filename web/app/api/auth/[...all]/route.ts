03f8d6a12b36d33f3aa0db098eac3565ae95af8b
Valerii Strilets <valerii.strilets@gmail.com>
2024-12-14 01:26:32 +0200

    update cors ad remove middleware

--- web/app/api/auth/[...all]/route.ts
+++ web/app/api/auth/[...all]/route.ts
@@ -1,4 +1,14 @@
+import { env } from '@/env'
 import { auth } from '@/lib/auth'
 import { toNextJsHandler } from 'better-auth/next-js'
 
-export const { GET, POST } = toNextJsHandler(auth.handler)
+export const { GET, POST } = toNextJsHandler((r) => {
+  if (!r.url.startsWith(env.NEXT_PUBLIC_URL)) {
+    r.headers.set('Access-Control-Allow-Origin', process.env.NODE_ENV === 'production' ? 'tauri://localhost' : 'http://localhost:1420')
+    r.headers.set('Access-Control-Allow-Credentials', 'true')
+    r.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+    r.headers.set('Access-Control-Expose-Headers', 'Set-Auth-Token')
+  }
+
+  return auth.handler(r)
+})
--- web/next.config.ts
+++ web/next.config.ts
@@ -4,31 +4,6 @@ const nextConfig: NextConfig = {
   experimental: {
     reactCompiler: true,
   },
-  async headers() {
-    return [
-      {
-        source: '/api/:path*',
-        headers: [
-          {
-            key: 'Access-Control-Allow-Origin',
-            value: process.env.NODE_ENV === 'production' ? 'tauri://localhost' : 'http://localhost:1420',
-          },
-          {
-            key: 'Access-Control-Allow-Credentials',
-            value: 'true',
-          },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value: 'Content-Type, Authorization',
-          },
-          {
-            key: 'Access-Control-Expose-Headers',
-            value: 'Set-Auth-Token',
-          },
-        ],
-      },
-    ]
-  },
 }
 
 export default nextConfig
