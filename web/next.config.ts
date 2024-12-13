631ecba8f510204867ccda170f7be7170d0d408d
Valerii Strilets <valerii.strilets@gmail.com>
2024-12-14 01:16:36 +0200

    minor

--- web/next.config.ts
+++ web/next.config.ts
@@ -18,10 +18,6 @@ const nextConfig: NextConfig = {
             value: 'true',
           },
           {
-            key: 'Access-Control-Allow-Methods',
-            value: 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
-          },
-          {
             key: 'Access-Control-Allow-Headers',
             value: 'Content-Type, Authorization',
           },
