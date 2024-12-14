00746d0c05d345317c4a8c2493570b172f5044ec
Valerii Strilets <valerii.strilets@gmail.com>
2024-12-14 02:09:29 +0200

    update cors

--- web/app/api/auth/[...all]/route.ts
+++ web/app/api/auth/[...all]/route.ts
@@ -1,18 +1,4 @@
-import { env } from '@/env'
 import { auth } from '@/lib/auth'
 import { toNextJsHandler } from 'better-auth/next-js'
 
-export const { GET, POST } = toNextJsHandler(async (request) => {
-  const origin = request.headers.get('origin')
-
-  const response = await auth.handler(request)
-
-  if (origin && !origin.startsWith(env.NEXT_PUBLIC_URL)) {
-    response.headers.set('Access-Control-Allow-Origin', process.env.NODE_ENV === 'production' ? 'tauri://localhost' : 'http://localhost:1420')
-    response.headers.set('Access-Control-Allow-Credentials', 'true')
-    response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-    response.headers.set('Access-Control-Expose-Headers', 'Set-Auth-Token')
-  }
-
-  return response
-})
+export const { GET, POST } = toNextJsHandler(auth.handler)
--- web/next.config.ts
+++ web/next.config.ts
@@ -4,6 +4,40 @@ const nextConfig: NextConfig = {
   experimental: {
     reactCompiler: true,
   },
+  async headers() {
+    const origin = process.env.NODE_ENV === 'production' ? 'tauri://localhost' : 'http://localhost:1420'
+
+    return [
+      {
+        source: '/api/:path*',
+        has: [
+          {
+            type: 'header',
+            key: 'origin',
+            value: origin,
+          },
+        ],
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: origin,
+          },
+          {
+            key: 'Access-Control-Allow-Credentials',
+            value: 'true',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'Content-Type, Authorization',
+          },
+          {
+            key: 'Access-Control-Expose-Headers',
+            value: 'Set-Auth-Token',
+          },
+        ],
+      },
+    ]
+  },
 }
 
 export default nextConfig
