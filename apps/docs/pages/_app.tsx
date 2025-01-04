be852b99e40ce4931c8bf3176e90899be84fe931
Valerii Strilets <valerii.strilets@gmail.com>
2025-01-05 00:41:18 +0200

    add check types

--- apps/docs/pages/_app.tsx
+++ apps/docs/pages/_app.tsx
@@ -1,3 +1,4 @@
-export default function App({ Component, pageProps }) {
+// eslint-disable-next-line ts/no-explicit-any
+export default function App({ Component, pageProps }: { Component: React.ComponentType, pageProps: any }) {
   return <Component {...pageProps} />
 }
