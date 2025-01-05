d6e48a9f14a684cd188696aec312321db3f29aae
Valerii Strilets <valerii.strilets@gmail.com>
2025-01-05 02:21:55 +0200

    add public url

--- apps/web/lib/client.ts
+++ apps/web/lib/client.ts
@@ -2,8 +2,10 @@ import type { auth } from './auth'
 import { inferAdditionalFields } from 'better-auth/client/plugins'
 import { nextCookies } from 'better-auth/next-js'
 import { createAuthClient } from 'better-auth/react'
+import { env } from '~/env'
 
 export const authClient = createAuthClient({
+  baseURL: env.NEXT_PUBLIC_URL,
   plugins: [
     nextCookies(),
     inferAdditionalFields<typeof auth>(),
