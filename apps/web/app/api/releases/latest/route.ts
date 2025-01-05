f71186f28cc91548139005f290fde8bb22eb09a6
Valerii Strilets <valerii.strilets@gmail.com>
2025-01-06 00:41:47 +0200

    update latest release

--- apps/web/app/api/releases/latest/route.ts
+++ apps/web/app/api/releases/latest/route.ts
@@ -11,14 +11,11 @@ export async function GET() {
 
   return Response.json({
     version: '0.1.1',
-    notes: 'Some notes about the release',
-    pub_date: '2025-01-05T00:00:00Z',
     platforms: {
       'darwin-aarch64': {
         signature: 'dW50cnVzdGVkIGNvbW1lbnQ6IHNpZ25hdHVyZSBmcm9tIHRhdXJpIHNlY3JldCBrZXkKUlVSRnc3d0ZLSFJVaTRTNllFUzVCVHo3azFBVDZNLzdweGIwTnFxd2VRc3hUc0VaMWwxV2ltY3RLbFVnSFEwYkVVV1huMzZRcGlxVVI0SFZ5eUVhRDFNaFJqeUVBajJvZGdBPQp0cnVzdGVkIGNvbW1lbnQ6IHRpbWVzdGFtcDoxNzM2MDk1MTk5CWZpbGU6Q29ubm5lY3QuYXBwLnRhci5nego3MG5HMXhWSjFnVTlQZDIwOXJnM2VmbVM4NFdnam9JakF6S0dMN1Qra2dmNC9aSHBUUzJOcUp0Ynl4dlNUeDZaaldGaXZSRVF2c05JMzB4cy9SRHlCUT09Cg==',
         url: 'https://oywnxcvzfsqzhfwvavrd.supabase.co/storage/v1/object/public/releases/v1/0.1.0.tar.gz',
       },
     },
-  },
-  )
+  })
 }
