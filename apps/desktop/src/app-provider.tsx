ec3f835c99ea7641da45bf09a431524e17bf275c
Valerii Strilets <valerii.strilets@gmail.com>
2025-01-07 15:54:39 +0200

    update auth

--- apps/desktop/src/app-provider.tsx
+++ apps/desktop/src/app-provider.tsx
@@ -1,5 +1,7 @@
 import { useRouter } from '@tanstack/react-router'
-import { useEffect } from 'react'
+import posthog from 'posthog-js'
+import { useEffect, useRef } from 'react'
+import { toast } from 'sonner'
 import { useDeepLinksListener } from './deep-links'
 import { useSession } from './hooks/use-session'
 import { authClient } from './lib/auth'
@@ -7,13 +9,30 @@ import { authClient } from './lib/auth'
 export function AppProvider({ children }: { children: React.ReactNode }) {
   const { refetch, isAuthenticated, isLoading } = useSession()
   const router = useRouter()
+  const isFirstRender = useRef(true)
 
   useEffect(() => {
-    authClient.$store.listen('$sessionSignal', () => refetch())
+    authClient.$store.listen('$sessionSignal', async () => {
+      if (isFirstRender.current) {
+        isFirstRender.current = false
+        return
+      }
+
+      toast.info('session signal')
+      const { data } = await refetch()
+
+      if (data?.data) {
+        posthog.identify(data.data.user.id)
+      }
+      else {
+        posthog.reset()
+      }
+    })
   }, [])
 
   useEffect(() => {
     if (!isLoading) {
+      toast.info('redirect')
       router.navigate({ to: isAuthenticated ? '/' : '/sign-up' })
     }
   }, [isLoading, isAuthenticated])
