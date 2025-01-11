9a24fe10a5fedf918cc800f0ced0cf6eacf91265
Valerii Strilets <valerii.strilets@gmail.com>
2025-01-11 21:41:01 +0200

    ui fixes

--- packages/ui/tailwind.config.ts
+++ packages/ui/tailwind.config.ts
@@ -2,6 +2,7 @@ import type { Config } from 'tailwindcss'
 import animate from 'tailwindcss-animate'
 
 export default {
+  darkMode: 'class',
   content: [
     './index.html',
     '{app,components,src}/**/*.{ts,tsx}',
