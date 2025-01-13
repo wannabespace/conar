f0a8bd54e4cb11ca8179cf35fb54924f1f42db78
Valerii Strilets <valerii.strilets@gmail.com>
2025-01-13 17:40:53 +0200

    remove docs

--- apps/docs/pages/_app.tsx
+++
@@ -1,4 +0,0 @@
-// eslint-disable-next-line ts/no-explicit-any
-export default function App({ Component, pageProps }: { Component: React.ComponentType, pageProps: any }) {
-  return <Component {...pageProps} />
-}
--- apps/docs/theme.config.tsx
+++
@@ -1,8 +0,0 @@
-import type { useThemeConfig } from 'nextra-theme-docs'
-
-export default {
-  logo: <span>My Nextra Documentation</span>,
-  project: {
-    link: 'https://github.com/shuding/nextra',
-  },
-} satisfies Partial<ReturnType<typeof useThemeConfig>>
