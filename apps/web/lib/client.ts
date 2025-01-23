9deb693897157a2b322755026d4811046c7f0211
Valerii Strilets <valerii.strilets@gmail.com>
2025-01-23 18:30:26 +0200

    fix types

--- apps/web/lib/client.ts
+++ apps/web/lib/client.ts
@@ -4,7 +4,9 @@ import { nextCookies } from 'better-auth/next-js'
 import { createAuthClient } from 'better-auth/react'
 import { env } from '~/env'
 
-export const authClient = createAuthClient({
+type AuthClient = ReturnType<typeof createAuthClient>
+
+export const authClient: AuthClient = createAuthClient({
   baseURL: env.NEXT_PUBLIC_URL,
   plugins: [
     nextCookies(),
