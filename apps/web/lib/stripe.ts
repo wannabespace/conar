0e4818c1bbb4f3707b18401da557f204b8a06c31
Valerii Strilets <valerii.strilets@gmail.com>
2025-02-25 16:04:03 +0200

    update packages

--- apps/web/lib/stripe.ts
+++ apps/web/lib/stripe.ts
@@ -2,7 +2,7 @@ import Stripe from 'stripe'
 import { env } from '~/env'
 
 export const stripe = new Stripe(env.STRIPE_SECRET_KEY, {
-  apiVersion: '2025-01-27.acacia',
+  apiVersion: '2025-02-24.acacia',
   appInfo: {
     name: 'Connnect',
     url: env.NEXT_PUBLIC_URL,
