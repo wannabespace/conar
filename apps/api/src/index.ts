84ff556e64b111f81ed673c9d58d43d9fb4d232a
Valerii Strilets <valerii.strilets@gmail.com>
2025-03-18 21:24:49 +0200

    refactor: remove unused start command and update port parsing

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -32,7 +32,7 @@ app.use(
 
 serve({
   fetch: app.fetch,
-  port: process.env.PORT ? Number.parseInt(process.env.PORT) : 3000,
+  port: process.env.PORT ? Number(process.env.PORT) : 3000,
 }, (info) => {
   console.log(`Server is running on http://localhost:${info.port}`)
 })
