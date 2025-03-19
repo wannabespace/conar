4e95dd04562170a63b31bec5895e17c6179e606f
Valerii Strilets <valerii.strilets@gmail.com>
2025-03-19 17:38:08 +0200

    refactor: remove useless motion div

--- apps/desktop/src/routes/(protected)/_protected.tsx
+++ apps/desktop/src/routes/(protected)/_protected.tsx
@@ -28,22 +28,16 @@ function LayoutComponent() {
 
   return (
     <motion.div
-      initial={{ opacity: 0, y: -10 }}
-      animate={{ opacity: 1, y: 0 }}
-      exit={{ opacity: 0, y: 10 }}
+      initial={{ opacity: 0, scale: 1.1 }}
+      animate={{ opacity: 1, scale: 1 }}
+      exit={{ opacity: 0, scale: 0.9 }}
       transition={{ duration: 0.3 }}
       className="min-h-screen flex flex-col"
     >
       <Navbar />
-      <motion.div
-        className="flex flex-1 px-3"
-        initial={{ opacity: 0, scale: 1.1 }}
-        animate={{ opacity: 1, scale: 1 }}
-        exit={{ opacity: 0, scale: 0.9 }}
-        transition={{ duration: 0.3 }}
-      >
+      <div className="flex flex-1 px-3">
         <Outlet />
-      </motion.div>
+      </div>
     </motion.div>
   )
 }
