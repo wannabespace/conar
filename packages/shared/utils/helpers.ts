144798e71f88200152b8a62affa2614da1f5dd34
Valerii Strilets <valerii.strilets@gmail.com>
2025-03-19 16:27:01 +0200

    chore: add debounce function to shared

--- packages/shared/utils/helpers.ts
+++ packages/shared/utils/helpers.ts
@@ -1,3 +1,14 @@
 export function enumValues<T extends { [key: string]: string }>(enumType: T) {
   return Object.values(enumType) as [T[keyof T], ...T[keyof T][]]
 }
+
+export function debounce<F extends (...args: Parameters<F>) => ReturnType<F>>(func: F, waitFor: number) {
+  let timeout: ReturnType<typeof setTimeout>
+
+  const debounced = (...args: Parameters<F>) => {
+    clearTimeout(timeout)
+    timeout = setTimeout(() => func(...args), waitFor)
+  }
+
+  return debounced
+}
