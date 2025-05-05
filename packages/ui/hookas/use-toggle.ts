5562d16682dfeee2d5e05e976d5155a40f7e5e35
Valerii Strilets <valerii.strilets@gmail.com>
2025-05-06 00:51:35 +0300

    refactor(actions center): add dashboard

---
+++ packages/ui/hookas/use-toggle.ts
@@ -0,0 +1,11 @@
+import * as React from 'react'
+
+export function useToggle(initialValue = false) {
+  const [value, setValue] = React.useState(initialValue)
+
+  const toggle = React.useCallback((value?: boolean) => {
+    setValue(prev => value ?? !prev)
+  }, [])
+
+  return [value, toggle] as const
+}
