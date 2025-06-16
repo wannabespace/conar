ad2f6ae5428369d64d26494367377be4cb2b2f70
Valerii Strilets <strilets.v@novapay.ua>
2025-06-17 00:46:33 +0300

    refactor: remove unnecessary env variables from dev

--- apps/api/src/env.ts
+++ apps/api/src/env.ts
@@ -1,21 +1,51 @@
 import { type } from 'arktype'
 
+const isProduction = process.env.NODE_ENV === 'production'
+
 const envType = type({
-  API_URL: 'string > 1',
-  WEB_URL: 'string > 1',
-  DATABASE_URL: 'string > 1',
-  ENCRYPTION_SECRET: 'string > 1',
-  BETTER_AUTH_SECRET: 'string > 1',
-  STRIPE_SECRET_KEY: 'string > 1',
-  LOOPS_API_KEY: 'string > 1',
-  OPENAI_API_KEY: 'string > 1',
-  ANTHROPIC_API_KEY: 'string > 1',
-  GOOGLE_GENERATIVE_AI_API_KEY: 'string > 1',
-  XAI_API_KEY: 'string > 1',
-  GOOGLE_CLIENT_ID: 'string > 1',
-  GOOGLE_CLIENT_SECRET: 'string > 1',
-  GITHUB_CLIENT_ID: 'string > 1',
-  GITHUB_CLIENT_SECRET: 'string > 1',
+  API_URL: 'string',
+  WEB_URL: 'string',
+  DATABASE_URL: 'string',
+  ENCRYPTION_SECRET: 'string',
+  BETTER_AUTH_SECRET: 'string',
+  STRIPE_SECRET_KEY: 'string',
+  LOOPS_API_KEY: 'string',
+  OPENAI_API_KEY: 'string',
+  ANTHROPIC_API_KEY: 'string',
+  GOOGLE_GENERATIVE_AI_API_KEY: 'string',
+  XAI_API_KEY: 'string',
+  GOOGLE_CLIENT_ID: 'string',
+  GOOGLE_CLIENT_SECRET: 'string',
+  GITHUB_CLIENT_ID: 'string',
+  GITHUB_CLIENT_SECRET: 'string',
 })
 
-export const env = envType.assert(process.env)
+const devOptionalEnvs = [
+  'STRIPE_SECRET_KEY',
+  'LOOPS_API_KEY',
+  'OPENAI_API_KEY',
+  'ANTHROPIC_API_KEY',
+  'GOOGLE_GENERATIVE_AI_API_KEY',
+  'XAI_API_KEY',
+  'GOOGLE_CLIENT_ID',
+  'GOOGLE_CLIENT_SECRET',
+  'GITHUB_CLIENT_ID',
+  'GITHUB_CLIENT_SECRET',
+] satisfies (keyof typeof envType.infer)[]
+
+devOptionalEnvs.forEach((env) => {
+  if (!process.env[env]) {
+    console.warn(`"${env}" is not set in env file`)
+  }
+})
+
+export const env = isProduction
+  ? envType.assert(process.env)
+  : envType
+      .merge(
+        devOptionalEnvs.reduce((acc, env) => {
+          acc[`${env}?`] = 'string'
+          return acc
+        }, {} as { [K in `${typeof devOptionalEnvs[number]}?`]: 'string' }),
+      )
+      .assert(process.env)
--- apps/api/src/lib/stripe.ts
+++ apps/api/src/lib/stripe.ts
@@ -1,7 +1,7 @@
 import Stripe from 'stripe'
 import { env } from '~/env'
 
-export const stripe = new Stripe(env.STRIPE_SECRET_KEY, {
+export const stripe = new Stripe(env.STRIPE_SECRET_KEY!, {
   apiVersion: '2025-05-28.basil',
   appInfo: {
     name: 'Conar',
