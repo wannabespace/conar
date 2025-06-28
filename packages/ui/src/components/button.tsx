602945c12e8a48d741d4a46ed06a1fbe0a7d0efa
Valerii Strilets <valerii.strilets@gmail.com>
2025-06-28 17:59:46 +0300

    refactor: add active state to button

--- packages/ui/src/components/button.tsx
+++ packages/ui/src/components/button.tsx
@@ -7,7 +7,7 @@ import { cva } from 'class-variance-authority'
 const beforeClasses = 'before:absolute before:block before:rounded-full before:blur-xl before:h-full before:-top-3 before:opacity-50 before:left-1/2 before:-translate-x-1/2 before:h-5 before:w-1/3'
 
 const buttonVariants = cva(
-  'relative overflow-hidden cursor-pointer inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*=\'size-\'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive',
+  'relative overflow-hidden cursor-pointer inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 active:scale-97 [&_svg]:pointer-events-none [&_svg:not([class*=\'size-\'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive',
   {
     variants: {
       variant: {
