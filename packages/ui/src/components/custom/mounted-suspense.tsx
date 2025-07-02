83f1d52460b61463b7d97c621c9d10b1af691c78
Valerii Strilets <valerii.strilets@gmail.com>
2025-07-02 16:45:30 +0300

    chore: update packages

--- packages/ui/src/components/custom/mounted-suspense.tsx
+++ packages/ui/src/components/custom/mounted-suspense.tsx
@@ -1,5 +1,6 @@
+import type { ComponentProps } from 'react'
 import { useIsMounted } from '@conar/ui/hookas/use-is-mounted'
-import { type ComponentProps, Suspense } from 'react'
+import { Suspense } from 'react'
 
 export function MountedSuspense({ fallback, children, ...props }: ComponentProps<typeof Suspense>) {
   const isMounted = useIsMounted()
