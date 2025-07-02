09e2f564c5193fc43b89282999ad1c950e8e3211
Valerii Strilets <valerii.strilets@gmail.com>
2025-07-02 20:47:20 +0300

    refactor: update monaco workers

--- apps/desktop/src/monaco-worker.ts
+++ apps/desktop/src/monaco-worker.ts
@@ -1,9 +1,8 @@
 /* eslint-disable no-restricted-globals, new-cap */
 import * as monaco from 'monaco-editor'
 import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker'
-import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker'
-import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker'
 import jsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker'
+import sqlWorker from 'monaco-editor/esm/vs/language/sql/sql.worker?worker'
 import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker'
 
 self.MonacoEnvironment = {
@@ -11,11 +10,8 @@ self.MonacoEnvironment = {
     if (label === 'json') {
       return new jsonWorker()
     }
-    if (label === 'css' || label === 'scss' || label === 'less') {
-      return new cssWorker()
-    }
-    if (label === 'html' || label === 'handlebars' || label === 'razor') {
-      return new htmlWorker()
+    if (label === 'sql') {
+      return new sqlWorker()
     }
     if (label === 'typescript' || label === 'javascript') {
       return new tsWorker()
