2db1d8212b4eb2a5e2fdb81fb28afefe41d4d8a0
Valerii Strilets <valerii.strilets@gmail.com>
2025-07-03 01:15:24 +0300

    chore: downgrade deps

--- apps/api/src/lib/stripe.ts
+++ apps/api/src/lib/stripe.ts
@@ -2,7 +2,7 @@ import Stripe from 'stripe'
 import { env } from '~/env'
 
 export const stripe = new Stripe(env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2025-06-30.basil',
+  apiVersion: '2025-05-28.basil',
   appInfo: {
     name: 'Conar',
     url: env.WEB_URL,
