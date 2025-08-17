29f5cb36d5ce8e55e7eed1e78b372116a7e5356d
Valerii Strilets <strilets.v@novapay.ua>
2025-08-17 15:57:07 +0300

    chore: minor

--- apps/desktop/src/components/cell-switch.tsx
+++ apps/desktop/src/components/cell-switch.tsx
@@ -2,6 +2,8 @@ import { getOS } from '@conar/shared/utils/os'
 import { Switch } from '@conar/ui/components/switch'
 import { useKeyboardEvent } from '@react-hookz/web'
 
+const os = getOS(navigator.userAgent)
+
 export function CellSwitch({
   checked,
   onChange,
@@ -11,14 +13,12 @@ export function CellSwitch({
   onChange: (checked: boolean) => void
   onSave: (value: string) => void
 }) {
-  const os = getOS(navigator.userAgent)
   useKeyboardEvent(e => e.key === 'Enter' && (os.type === 'macos' ? e.metaKey : e.ctrlKey), () => onSave(checked ? 'true' : 'false'))
 
   return (
     <div className="flex gap-2 items-center text-xs">
       <Switch checked={checked} onCheckedChange={onChange} />
       <code className="font-mono">{checked.toString()}</code>
-
     </div>
   )
 }
