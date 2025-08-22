924377f15c285d5668283f9debbe35ce8e59c0c2
Valerii Strilets <strilets.v@novapay.ua>
2025-08-22 15:32:06 +0300

    chore: add redis mock

--- apps/api/src/lib/redis.ts
+++ apps/api/src/lib/redis.ts
@@ -1,15 +1,7 @@
 import Redis from 'ioredis'
+import RedisMock from 'ioredis-mock'
 import { env } from '~/env'
 
-export const redis = env.REDIS_URL
+export const redis: Redis = env.REDIS_URL
   ? new Redis(env.REDIS_URL)
-  : {
-      get: () => {
-        console.warn('Redis is not configured, returning null')
-        return Promise.resolve(null)
-      },
-      set: () => {
-        console.warn('Redis is not configured, returning null')
-        return Promise.resolve(null)
-      },
-    } as unknown as Redis
+  : new RedisMock()
