301e63c8799a202551c4223c0054c39bc07f5c83
Valerii Strilets <valerii.strilets@gmail.com>
2025-08-29 13:39:14 +0300

    feat: add posthog error tracking

--- apps/desktop/vite.config.ts
+++ apps/desktop/vite.config.ts
@@ -7,12 +7,8 @@ import electron from 'vite-plugin-electron/simple'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import pkg from './package.json'
 
-export default defineConfig(({ command }) => {
+export default defineConfig(() => {
   fs.rmSync('dist-electron', { recursive: true, force: true })
-
-  const isServe = command === 'serve'
-  const isBuild = command === 'build'
-  const sourcemap = isServe || !!process.env.VSCODE_DEBUG
 
   return {
     plugins: [
@@ -29,8 +25,6 @@ export default defineConfig(({ command }) => {
           entry: 'electron/main/index.ts',
           vite: {
             build: {
-              sourcemap,
-              minify: isBuild,
               outDir: 'dist-electron/main',
               rollupOptions: {
                 external: Object.keys('dependencies' in pkg ? pkg.dependencies : {}),
@@ -42,8 +36,6 @@ export default defineConfig(({ command }) => {
           input: 'electron/preload/index.ts',
           vite: {
             build: {
-              sourcemap: sourcemap ? 'inline' : undefined,
-              minify: isBuild,
               outDir: 'dist-electron/preload',
               rollupOptions: {
                 external: Object.keys('dependencies' in pkg ? pkg.dependencies : {}),
@@ -53,6 +45,9 @@ export default defineConfig(({ command }) => {
         },
       }),
     ],
+    build: {
+      sourcemap: true,
+    },
     optimizeDeps: {
       exclude: ['@electric-sql/pglite'],
     },
