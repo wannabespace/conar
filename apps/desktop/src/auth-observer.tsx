b0addfe37ce0391a6604f79525c913afd688a12e
Valerii Strilets <valerii.strilets@gmail.com>
2025-09-02 01:18:32 +0300

    refactor: improve error handling

--- apps/desktop/src/auth-observer.tsx
+++ apps/desktop/src/auth-observer.tsx
@@ -1,15 +1,16 @@
 import { useAsyncEffect } from '@conar/ui/hookas/use-async-effect'
 import { useLocation, useRouter } from '@tanstack/react-router'
 import { useEffect } from 'react'
+import { toast } from 'sonner'
 import { identifyUser } from '~/lib/events'
-import { authClient } from './lib/auth'
+import { authClient, bearerToken } from './lib/auth'
 import { handleDeepLink } from './lib/deep-links'
 
 const authRoutes = ['/sign-in', '/sign-up']
 const publicRoutes = [...authRoutes]
 
 export function AuthObserver() {
-  const { data, isPending, refetch } = authClient.useSession()
+  const { data, error, isPending, refetch } = authClient.useSession()
   const router = useRouter()
   const location = useLocation()
 
@@ -28,6 +29,16 @@ export function AuthObserver() {
   useEffect(() => {
     if (isPending)
       return
+
+    /**
+     * An error can be only on the server side
+     * To not block the app, we navigate to the home page to continue working
+     */
+    if (error && bearerToken.get()) {
+      router.navigate({ to: '/' })
+      toast.error('Something went wrong with our server. You can continue working, but some features may not work as expected.')
+      return
+    }
 
     if (data?.user && authRoutes.includes(location.pathname)) {
       router.navigate({ to: '/' })
