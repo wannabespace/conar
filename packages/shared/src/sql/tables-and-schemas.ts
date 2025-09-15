29469b1056fb125042cdc71712a681468141640a
Valerii Strilets <valerii.strilets@gmail.com>
2025-09-15 12:39:00 +0300

    refactor: improve table and schemas query

--- packages/shared/src/sql/tables-and-schemas.ts
+++ packages/shared/src/sql/tables-and-schemas.ts
@@ -3,19 +3,19 @@ import { prepareSql } from '@conar/shared/utils/helpers'
 import { type } from 'arktype'
 
 export const tablesAndSchemasType = type({
-  schema: 'string',
-  table: 'string',
-}).array().pipe(tables => Object.entries(Object.groupBy(tables, table => table.schema)).map(([schema, tables]) => ({
+  table_schema: 'string',
+  table_name: 'string',
+}).array().pipe(tables => Object.entries(Object.groupBy(tables, table => table.table_schema)).map(([schema, tables]) => ({
   name: schema,
-  tables: tables!.map(table => table.table),
+  tables: tables!.map(table => table.table_name),
 })))
 
 export function tablesAndSchemasSql(): Record<DatabaseType, string> {
   return {
     postgres: prepareSql(`
       SELECT
-        "table_schema" as schema,
-        "table_name" as table
+        "table_schema",
+        "table_name"
       FROM "information_schema"."tables"
       WHERE "table_schema" NOT IN ('pg_catalog', 'information_schema')
         AND "table_schema" NOT LIKE 'pg_toast%'
