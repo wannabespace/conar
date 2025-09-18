68ae60509337a645fe937f81dbbcfabceeac226f
Valerii Strilets <valerii.strilets@gmail.com>
2025-09-18 19:00:58 +0300

    refactor: add tanstack devtools

--- apps/desktop/src/routes/__root.tsx
+++ apps/desktop/src/routes/__root.tsx
@@ -1,17 +1,18 @@
 import { title } from '@conar/shared/utils/title'
 import { Toaster } from '@conar/ui/components/sonner'
 import { ThemeProvider } from '@conar/ui/theme-provider'
+import { TanStackDevtools } from '@tanstack/react-devtools'
 import { QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { ReactQueryDevtoolsPanel } from '@tanstack/react-query-devtools'
 import { createRootRoute, HeadContent, Outlet } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
+import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
 import { useEffect } from 'react'
 import { AuthObserver } from '~/auth-observer'
 import { enterAppAnimation } from '~/enter'
 import { ErrorPage } from '~/error-page'
 import { authClient } from '~/lib/auth'
 import { EventsProvider } from '~/lib/events'
-import { queryClient } from '~/main'
+import { queryClient, router } from '~/main'
 import { checkForUpdates, UpdatesObserver } from '~/updates-observer'
 
 export const Route = createRootRoute({
@@ -45,10 +46,18 @@ function RootDocument() {
             <Outlet />
             <Toaster />
             {import.meta.env.DEV && (
-              <>
-                <TanStackRouterDevtools position="bottom-right" />
-                <ReactQueryDevtools initialIsOpen={false} />
-              </>
+              <TanStackDevtools
+                plugins={[
+                  {
+                    name: 'TanStack Query',
+                    render: <ReactQueryDevtoolsPanel />,
+                  },
+                  {
+                    name: 'TanStack Router',
+                    render: <TanStackRouterDevtoolsPanel router={router} />,
+                  },
+                ]}
+              />
             )}
           </QueryClientProvider>
         </ThemeProvider>
