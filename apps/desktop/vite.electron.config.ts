3a02914316a8b540af8c253b45e83b17b8192b23
Valerii Strilets <valerii.strilets@gmail.com>
2025-09-19 17:59:11 +0300

    refactor: change ports paths

--- apps/desktop/vite.electron.config.ts
+++ apps/desktop/vite.electron.config.ts
@@ -1,8 +1,8 @@
 import { dirname, resolve } from 'node:path'
 import { fileURLToPath } from 'node:url'
-import { PORTS } from '@conar/shared/constants'
 import { defineConfig, defineViteConfig, externalizeDepsPlugin, mergeConfig } from 'electron-vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import { PORTS } from '../../packages/shared/src/constants'
 import { viteBaseConfig } from './configs/vite.base.config'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,9 +1,9 @@
-import { PORTS } from '@conar/shared/constants'
 import tailwindcss from '@tailwindcss/vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import tsConfigPaths from 'vite-tsconfig-paths'
+import { PORTS } from '../../packages/shared/src/constants'
 
 export default defineConfig({
   plugins: [
