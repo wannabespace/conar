aac0886799ee323a3d9d96bf84b772d3cfb03fb1
Valerii Strilets <valerii.strilets@gmail.com>
2025-09-19 16:53:48 +0300

    refactor: improve constraints query

--- packages/shared/src/sql/constraints.ts
+++ packages/shared/src/sql/constraints.ts
@@ -14,10 +14,14 @@ export const constraintsType = type({
   constraint_name: 'string',
   constraint_type: constraintType,
   column_name: 'string | null',
+  foreign_table_name: 'string',
+  foreign_column_name: 'string',
 }).pipe(item => ({
   name: item.constraint_name,
   type: constraintTypeMap[item.constraint_type],
   column: item.column_name,
+  foreignTable: item.constraint_type === 'FOREIGN KEY' ? item.foreign_table_name : null,
+  foreignColumn: item.constraint_type === 'FOREIGN KEY' ? item.foreign_column_name : null,
 }))
 
 export function constraintsSql(schema: string, table: string): Record<DatabaseType, string> {
@@ -26,12 +30,17 @@ export function constraintsSql(schema: string, table: string): Record<DatabaseTy
       SELECT
         tc."constraint_name",
         tc."constraint_type",
-        kcu."column_name"
+        kcu."column_name",
+        ccu."table_name" AS "foreign_table_name",
+        ccu."column_name" AS "foreign_column_name"
       FROM "information_schema"."table_constraints" tc
       LEFT JOIN "information_schema"."key_column_usage" kcu
         ON tc."constraint_name" = kcu."constraint_name"
         AND tc."table_schema" = kcu."table_schema"
         AND tc."table_name" = kcu."table_name"
+      LEFT JOIN "information_schema"."constraint_column_usage" ccu
+        ON tc."constraint_name" = ccu."constraint_name"
+        AND tc."constraint_schema" = ccu."constraint_schema"
       WHERE tc."table_schema" = '${schema}'
         AND tc."table_name" = '${table}'
         AND tc."constraint_type" <> 'CHECK'
