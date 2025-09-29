f7f146c0beba3116b0b33f4c5751d32e422c9f06
Valerii Strilets <valerii.strilets@gmail.com>
2025-09-29 23:38:45 +0300

    chore: update packages

--- apps/desktop/src/monaco-worker.ts
+++ apps/desktop/src/monaco-worker.ts
@@ -1,26 +1,26 @@
-/* eslint-disable no-restricted-globals, new-cap */
 import * as monaco from 'monaco-editor'
-import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker'
-import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker'
-import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker'
-import jsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker'
-import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker'
+import EditorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker'
+import CssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker'
+import HtmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker'
+import JsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker'
+import TsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker'
 
-self.MonacoEnvironment = {
-  getWorker(_, label) {
+// @ts-expect-error wrong type
+globalThis.MonacoEnvironment = {
+  getWorker(_: unknown, label: string) {
     if (label === 'json') {
-      return new jsonWorker()
+      return new JsonWorker()
     }
     if (label === 'css' || label === 'scss' || label === 'less') {
-      return new cssWorker()
+      return new CssWorker()
     }
     if (label === 'html' || label === 'handlebars' || label === 'razor') {
-      return new htmlWorker()
+      return new HtmlWorker()
     }
     if (label === 'typescript' || label === 'javascript') {
-      return new tsWorker()
+      return new TsWorker()
     }
-    return new editorWorker()
+    return new EditorWorker()
   },
 }
 
