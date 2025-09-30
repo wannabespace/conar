b542b1b3be8f77ec9269174168e0e9e8dd0a4386
Valerii Strilets <valerii.strilets@gmail.com>
2025-09-30 15:31:02 +0300

    refactor: remove useless prefetches

--- apps/desktop/src/entities/database/lib/fetching.ts
+++ apps/desktop/src/entities/database/lib/fetching.ts
@@ -13,18 +13,10 @@ export async function prefetchDatabaseCore(database: typeof databases.$inferSele
     return
   }
 
-  const [tablesAndSchemas] = await Promise.all([
-    queryClient.ensureQueryData(tablesAndSchemasQuery({ database })),
+  await Promise.all([
+    queryClient.prefetchQuery(tablesAndSchemasQuery({ database })),
     queryClient.prefetchQuery(databaseEnumsQuery({ database })),
   ])
-
-  // This approach is needed to launch prefetch, but without overloading the database
-  for (const schemaAndTables of tablesAndSchemas.schemas) {
-    for (const table of schemaAndTables.tables) {
-      await queryClient.prefetchQuery(databaseTableColumnsQuery({ database, table, schema: schemaAndTables.name }))
-      await queryClient.prefetchQuery(databaseTableConstraintsQuery({ database, table, schema: schemaAndTables.name }))
-    }
-  }
 }
 
 export async function prefetchDatabaseTableCore({ database, schema, table, query }: {
