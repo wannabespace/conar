3d9cd632651090fb103f707e8134d5a4bdca47f6
Valerii Strilets <valerii.strilets@gmail.com>
2025-10-01 22:54:21 +0300

    refactor: fix redis url

--- apps/api/src/lib/redis.ts
+++ apps/api/src/lib/redis.ts
@@ -3,5 +3,5 @@ import RedisMock from 'ioredis-mock'
 import { env } from '~/env'
 
 export const redis: Redis = env.REDIS_URL
-  ? new Redis(env.REDIS_URL)
+  ? new Redis(`${env.REDIS_URL}?family=0`)
   : new RedisMock()
