7f4fe16c084cbb1ef73bcb7010aab4d248f64964
Valerii Strilets <valerii.strilets@gmail.com>
2025-11-19 11:55:01 +0200

    chore: minor

--- apps/desktop/src/entities/database/sql/select.ts
+++ apps/desktop/src/entities/database/sql/select.ts
@@ -5,28 +5,25 @@ import { buildWhere } from './rows'
 
 export const selectQuery = createQuery({
   type: type('Record<string, unknown>[]'),
-  query: ({ schema, table, select, filters}: { schema: string, table: string, select: string[], filters: ActiveFilter[] }) => ({
-    postgres: ({ db }) => {
-      const query = db
-        .withSchema(schema)
-        .withTables<{ [table]: Record<string, unknown> }>()
-        .selectFrom(table)
-        .select(select)
-        .where(eb => buildWhere(eb, filters))
-        .execute()
-
-      return query
-    },
-    mysql: ({ db }) => {
-      const query = db
-        .withSchema(schema)
-        .withTables<{ [table]: Record<string, unknown> }>()
-        .selectFrom(table)
-        .select(select)
-        .where(eb => buildWhere(eb, filters))
-        .execute()
-
-      return query
-    },
+  query: ({ schema, table, select, filters }: {
+    schema: string
+    table: string
+    select: string[]
+    filters: ActiveFilter[]
+  }) => ({
+    postgres: ({ db }) => db
+      .withSchema(schema)
+      .withTables<{ [table]: Record<string, unknown> }>()
+      .selectFrom(table)
+      .select(select)
+      .where(eb => buildWhere(eb, filters))
+      .execute(),
+    mysql: ({ db }) => db
+      .withSchema(schema)
+      .withTables<{ [table]: Record<string, unknown> }>()
+      .selectFrom(table)
+      .select(select)
+      .where(eb => buildWhere(eb, filters))
+      .execute(),
   }),
 })
