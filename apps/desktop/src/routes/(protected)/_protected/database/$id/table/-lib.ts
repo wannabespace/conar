af3c35269f10d8e9bfe61a15ec14a1f0a44a0695
Valerii Strilets <valerii.strilets@gmail.com>
2025-12-01 15:59:20 +0200

    refactor: simplify columns size mapping

--- apps/desktop/src/routes/(protected)/_protected/database/$id/table/-lib.ts
+++ apps/desktop/src/routes/(protected)/_protected/database/$id/table/-lib.ts
@@ -2,23 +2,22 @@ import { DEFAULT_COLUMN_WIDTH } from '~/entities/database'
 
 export const selectSymbol = Symbol('table-selection')
 
-const columnsSizeMap = new Map<string, number>([
-  ['boolean', 160],
-  ['number', 170],
-  ['int', 150],
-  ['uint', 150],
-  ['decimal', 180],
-  ['tinyint', 150],
-  ['decimal', 150],
-  ['integer', 150],
-  ['bigint', 170],
-  ['timestamp', 240],
-  ['datetime', 180],
-  ['timestamptz', 240],
-  ['float', 170],
-  ['uuid', 290],
-])
+const columnsSizeMap: Record<string, number> = {
+  boolean: 160,
+  number: 170,
+  int: 150,
+  uint: 150,
+  tinyint: 150,
+  decimal: 150,
+  integer: 150,
+  bigint: 170,
+  timestamp: 240,
+  datetime: 180,
+  timestamptz: 240,
+  float: 170,
+  uuid: 290,
+}
 
 export function getColumnSize(type: string): number {
-  return columnsSizeMap.entries().find(([key]) => type.toLowerCase().includes(key.toLowerCase()))?.[1] ?? DEFAULT_COLUMN_WIDTH
+  return Object.entries(columnsSizeMap).find(([key]) => type.toLowerCase().includes(key.toLowerCase()))?.[1] ?? DEFAULT_COLUMN_WIDTH
 }
