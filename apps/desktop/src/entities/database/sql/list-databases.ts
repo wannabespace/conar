a6f058bed6c996cd56f66a46fc25e16c5e75fb02
Rudra-Sankha-Sinhamahapatra <rssmp120@gmail.com>
2025-12-23 00:09:33 +0530

    fix: skip default databases

--- apps/desktop/src/entities/database/sql/list-databases.ts
+++ apps/desktop/src/entities/database/sql/list-databases.ts
@@ -17,6 +17,7 @@ export const listDatabasesQuery = createQuery({
     mysql: db => db
       .selectFrom('information_schema.SCHEMATA')
       .select('SCHEMA_NAME')
+      .where('SCHEMA_NAME', 'not in', ['information_schema', 'mysql', 'performance_schema', 'sys'])
       .orderBy('SCHEMA_NAME')
       .execute()
       .then(rows => rows.map(r => r.SCHEMA_NAME)),
@@ -24,6 +25,7 @@ export const listDatabasesQuery = createQuery({
     mssql: db => db
       .selectFrom('sys.databases')
       .select('name')
+      .where('name', 'not in', ['master', 'model', 'msdb', 'tempdb'])
       .orderBy('name')
       .execute()
       .then(rows => rows.map(r => r.name)),
@@ -31,6 +33,7 @@ export const listDatabasesQuery = createQuery({
     clickhouse: db => db
       .selectFrom('system.databases')
       .select('name')
+      .where('name', 'not in', ['INFORMATION_SCHEMA', 'information_schema', 'system'])
       .orderBy('name')
       .execute()
       .then(rows => rows.map(r => r.name)),
