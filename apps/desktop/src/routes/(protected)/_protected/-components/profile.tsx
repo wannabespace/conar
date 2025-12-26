737c3551706b7f287771ff6c1304215a4a8cdfd4
Valerii Strilets <valerii.strilets@gmail.com>
2025-12-26 12:06:32 +0200

    feat: enhance profile component with alert and layout adjustments

--- apps/desktop/src/routes/(protected)/_protected/-components/profile.tsx
+++ apps/desktop/src/routes/(protected)/_protected/-components/profile.tsx
@@ -1,11 +1,11 @@
 import { uppercaseFirst } from '@conar/shared/utils/helpers'
+import { Alert, AlertDescription } from '@conar/ui/components/alert'
 import { Badge } from '@conar/ui/components/badge'
 import { Button } from '@conar/ui/components/button'
 import { LoadingContent } from '@conar/ui/components/custom/loading-content'
 import { Skeleton } from '@conar/ui/components/skeleton'
-import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@conar/ui/components/tooltip'
 import { cn } from '@conar/ui/lib/utils'
-import { RiLogoutCircleRLine, RiVipCrownLine } from '@remixicon/react'
+import { RiArrowUpDoubleLine, RiLogoutCircleRLine, RiSettingsLine } from '@remixicon/react'
 import { UserAvatar, useSignOut } from '~/entities/user'
 import { useSubscription } from '~/entities/user/hooks/use-subscription'
 import { appStore } from '~/store'
@@ -15,63 +15,71 @@ export function Profile({ className }: { className?: string }) {
   const { subscription } = useSubscription()
 
   return (
-    <div className={cn('flex flex-row items-center justify-between', className)}>
-      <div className="flex flex-row items-center gap-4">
-        <UserAvatar className="size-16" fallbackClassName="text-2xl" />
-        <div>
-          {data?.user
-            ? (
-                <>
-                  <div className="flex items-center gap-2">
+    <div className={cn('flex flex-col gap-4', className)}>
+      <div className="flex flex-row items-center justify-between">
+        <div className="flex flex-row items-center gap-4">
+          <UserAvatar className="size-16" fallbackClassName="text-2xl" />
+          <div>
+            {data?.user
+              ? (
+                  <>
                     <h3 className="text-2xl font-semibold">{data.user.name || 'User'}</h3>
-                    <Badge
-                      variant="secondary"
-                    >
-                      {subscription?.plan ? uppercaseFirst(subscription.plan) : 'Hobby'}
-                    </Badge>
-                  </div>
-                  <p className="text-sm text-muted-foreground">{data.user.email}</p>
-                </>
-              )
-            : (
-                <>
-                  <div className="space-y-2">
-                    <Skeleton className="h-6 w-50 bg-accent/70" />
-                    <Skeleton className="h-4 w-32 bg-accent/70" />
-                  </div>
-                </>
-              )}
+                    <p className="text-sm text-muted-foreground">{data.user.email}</p>
+                  </>
+                )
+              : (
+                  <>
+                    <div className="space-y-2">
+                      <Skeleton className="h-6 w-50 bg-accent/70" />
+                      <Skeleton className="h-4 w-32 bg-accent/70" />
+                    </div>
+                  </>
+                )}
+          </div>
+        </div>
+        <div className="flex items-center gap-2">
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => signOut()}
+            disabled={isSigningOut}
+          >
+            <LoadingContent loading={isSigningOut}>
+              <RiLogoutCircleRLine className="size-4" />
+              Sign out
+            </LoadingContent>
+          </Button>
         </div>
       </div>
-      <div className="flex items-center gap-2">
-        <Button
-          variant={subscription ? 'outline' : 'secondary'}
-          size="sm"
-          onClick={() => appStore.setState(state => ({ ...state, subscriptionModalIsOpen: true } satisfies typeof state))}
-        >
-          <RiVipCrownLine className="size-4" />
-          {subscription ? 'Manage' : 'Upgrade'}
-        </Button>
-        <TooltipProvider>
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <Button
-                variant="outline"
-                size="icon-sm"
-                onClick={() => signOut()}
-                disabled={isSigningOut}
-              >
-                <LoadingContent loading={isSigningOut}>
-                  <RiLogoutCircleRLine />
-                </LoadingContent>
-              </Button>
-            </TooltipTrigger>
-            <TooltipContent>
-              Sign out
-            </TooltipContent>
-          </Tooltip>
-        </TooltipProvider>
-      </div>
+      <Alert className="px-2 py-1">
+        <AlertDescription className="flex items-center justify-between">
+          <span>
+            You are
+            {' '}
+            {subscription ? 'already' : 'currently'}
+            {' '}
+            on the
+            {' '}
+            <Badge
+              variant="secondary"
+            >
+              {subscription ? uppercaseFirst(subscription.plan) : 'Hobby'}
+            </Badge>
+            {' '}
+            plan.
+          </span>
+          <Button
+            variant="ghost"
+            size="sm"
+            onClick={() => appStore.setState(state => ({ ...state, subscriptionModalIsOpen: true } satisfies typeof state))}
+          >
+            {subscription
+              ? <RiSettingsLine className="size-4" />
+              : <RiArrowUpDoubleLine className="size-4" />}
+            {subscription ? 'Manage' : 'Upgrade'}
+          </Button>
+        </AlertDescription>
+      </Alert>
     </div>
   )
 }
