81115ac117c37b12b11b16c37ca797947fb76e5e
Valerii Strilets <valerii.strilets@gmail.com>
2026-01-08 01:21:09 +0200

    feat: integrate tanstackStartCookies into auth client

--- apps/web/src/lib/auth.ts
+++ apps/web/src/lib/auth.ts
@@ -2,6 +2,7 @@ import { createIsomorphicFn } from '@tanstack/react-start'
 import { getRequestHeaders } from '@tanstack/react-start/server'
 import { lastLoginMethodClient, magicLinkClient, organizationClient, twoFactorClient } from 'better-auth/client/plugins'
 import { createAuthClient } from 'better-auth/react'
+import { tanstackStartCookies } from 'better-auth/tanstack-start'
 
 export const authClient = createAuthClient({
   baseURL: import.meta.env.VITE_PUBLIC_API_URL,
@@ -11,6 +12,7 @@ export const authClient = createAuthClient({
     twoFactorClient(),
     magicLinkClient(),
     lastLoginMethodClient(),
+    tanstackStartCookies(),
   ],
 })
 
