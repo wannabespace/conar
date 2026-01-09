445d5d49120e20447f241b2812ee99dab7c99215
Rudra-Sankha-Sinhamahapatra <rssmp120@gmail.com>
2026-01-09 15:22:26 +0530

    fix: boolean value updation to false from null

--- apps/desktop/src/routes/_protected/database/$id/table/-components/table.tsx
+++ apps/desktop/src/routes/_protected/database/$id/table/-components/table.tsx
@@ -29,6 +29,9 @@ function prepareValue(value: unknown, column?: Column): unknown {
   if (!column)
     return value
 
+  if (column.type === 'boolean' && !column.isArray && value === '')
+    return false
+
   return typeof value === 'string' && column.isArray
     ? JSON.parse(value)
     : value
