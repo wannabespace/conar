1b3ad0b977b390b3f7250b23b82f9b122c749cc5
Harsh Vardhan Pandey <hp424805@gmail.com>
2026-01-28 04:15:10 +0530

    feat: add select multiple rows with shift (#224)
    Co-authored-by: Valerii Strilets <valerii.strilets@gmail.com>
    Co-authored-by: Rudra-Sankha-Sinhamahapatra <rssmp120@gmail.com>

--- apps/desktop/src/components/table/index.ts
+++ apps/desktop/src/components/table/index.ts
@@ -10,6 +10,11 @@ export * from './header'
 export * from './provider'
 export * from './table'
 export * from './table-context'
+export {
+  type SelectionState,
+  useShiftSelectionKeyDown,
+  type UseShiftSelectionKeyDownOptions,
+} from './use-shift-selection-key-down'
 
 export interface TableCellProps extends Pick<ColumnRenderer, 'size' | 'id'> {
   style: CSSProperties
