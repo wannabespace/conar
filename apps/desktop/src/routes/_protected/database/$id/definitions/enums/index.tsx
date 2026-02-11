aeb5d3aae8f6df41f7583e37da29fafcd12566b6
balanced <ifeelbalanced@gmail.com>
2026-02-12 00:16:28 +0200

    fix: loaderData connection head

--- apps/desktop/src/routes/_protected/database/$id/definitions/enums/index.tsx
+++ apps/desktop/src/routes/_protected/database/$id/definitions/enums/index.tsx
@@ -20,7 +20,7 @@ export const Route = createFileRoute('/_protected/database/$id/definitions/enums
   component: DatabaseEnumsPage,
   loader: ({ context }) => ({ connection: context.connection }),
   head: ({ loaderData }) => ({
-    meta: loaderData ? [{ title: title('Enums', loaderData.connection.name) }] : [],
+    meta: loaderData?.connection ? [{ title: title('Enums', loaderData.connection.name) }] : [],
   }),
 })
 
