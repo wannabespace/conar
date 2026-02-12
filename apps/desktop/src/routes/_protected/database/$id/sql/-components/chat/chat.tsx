95e524c2f1b9617c8d6f61789cb7e544f96f0959
Valerii Strilets <valerii.strilets@gmail.com>
2026-02-12 18:55:34 +0200

    refactor: change new chat hotkey to the chat block

--- apps/desktop/src/routes/_protected/database/$id/sql/-components/chat/chat.tsx
+++ apps/desktop/src/routes/_protected/database/$id/sql/-components/chat/chat.tsx
@@ -1,6 +1,8 @@
-import type { ComponentProps } from 'react'
 import { useChat } from '@ai-sdk/react'
+import { isCtrlAndKey } from '@conar/shared/utils/os'
+import { useKeyboardEvent } from '@conar/ui/hookas/use-keyboard-event'
 import { cn } from '@conar/ui/lib/utils'
+import { useRouter } from '@tanstack/react-router'
 import { useEffect, useRef } from 'react'
 import { useSubscription } from '~/entities/user/hooks'
 import { Route } from '../..'
@@ -9,10 +11,12 @@ import { ChatHeader } from './chat-header'
 import { ChatMessages } from './chat-messages'
 import { ChatPlaceholder } from './chat-placeholder'
 
-export function Chat({ className, ...props }: Omit<ComponentProps<'div'>, 'ref'>) {
+export function Chat({ className }: { className?: string }) {
   const { chat } = Route.useLoaderData()
+  const { connection } = Route.useRouteContext()
   const { messages, error } = useChat({ chat })
   const { subscription } = useSubscription()
+  const router = useRouter()
   const elementRef = useRef<HTMLDivElement>(null)
 
   useEffect(() => {
@@ -21,12 +25,23 @@ export function Chat({ className, ...props }: Omit<ComponentProps<'div'>, 'ref'>
     }
   }, [chat, subscription])
 
+  useKeyboardEvent(e => isCtrlAndKey(e, 'n'), () => {
+    router.navigate({
+      to: '/database/$id/sql',
+      params: { id: connection.id },
+      search: { chatId: undefined },
+    })
+  }, {
+    target: elementRef,
+    deps: [chat.id],
+  })
+
   return (
     <div
       key={chat.id}
       className={cn('relative flex flex-col justify-between gap-4 p-4', className)}
       ref={elementRef}
-      {...props}
+      tabIndex={0}
     >
       <ChatHeader chatId={chat.id} />
       {messages.length === 0 && !error && (
