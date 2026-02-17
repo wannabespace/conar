bf5287a2b1c91ff6bbfafd67a64b9c1523d6e267
Valerii Strilets <valerii.strilets@gmail.com>
2026-02-17 18:21:33 +0200

    chore: update fetch options

--- apps/web/src/lib/auth.ts
+++ apps/web/src/lib/auth.ts
@@ -3,13 +3,16 @@ import { getRequest } from '@tanstack/react-start/server'
 import { lastLoginMethodClient, magicLinkClient, organizationClient, twoFactorClient } from 'better-auth/client/plugins'
 import { createAuthClient } from 'better-auth/react'
 
-export const getAuthHeadersIsomorphic = createIsomorphicFn()
+const getAuthHeadersIsomorphic = createIsomorphicFn()
   .server(() => {
     const request = getRequest()
 
-    return request.headers
+    return {
+      'user-agent': request.headers.get('user-agent') ?? '',
+      'cookie': request.headers.get('cookie') ?? '',
+    }
   })
-  .client(() => new Headers())
+  .client(() => ({}))
 
 export const authClient = createAuthClient({
   baseURL: import.meta.env.VITE_PUBLIC_API_URL,
@@ -18,9 +21,9 @@ export const authClient = createAuthClient({
     onRequest: (request) => {
       const headers = getAuthHeadersIsomorphic()
 
-      headers.forEach((value, key) => {
+      for (const [key, value] of Object.entries(headers)) {
         request.headers.set(key, value)
-      })
+      }
 
       return request
     },
