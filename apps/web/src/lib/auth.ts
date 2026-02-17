55bb2cf521601676e93599381caa5876c2418f48
Valerii Strilets <valerii.strilets@gmail.com>
2026-02-17 18:04:22 +0200

    refactor: new better auth fetch option approach

--- apps/web/src/lib/auth.ts
+++ apps/web/src/lib/auth.ts
@@ -3,9 +3,31 @@ import { getRequest } from '@tanstack/react-start/server'
 import { lastLoginMethodClient, magicLinkClient, organizationClient, twoFactorClient } from 'better-auth/client/plugins'
 import { createAuthClient } from 'better-auth/react'
 
+export const getAuthHeadersIsomorphic = createIsomorphicFn()
+  .server(() => {
+    const request = getRequest()
+
+    return {
+      'user-agent': request.headers.get('user-agent') ?? '',
+      'cookie': request.headers.get('cookie') ?? '',
+    }
+  })
+  .client(() => ({}))
+
 export const authClient = createAuthClient({
   baseURL: import.meta.env.VITE_PUBLIC_API_URL,
   basePath: '/auth',
+  fetchOptions: {
+    onRequest: (request) => {
+      const headers = getAuthHeadersIsomorphic()
+
+      for (const [key, value] of Object.entries(headers)) {
+        request.headers.set(key, value)
+      }
+
+      return request
+    },
+  },
   plugins: [
     organizationClient(),
     twoFactorClient(),
@@ -14,17 +36,4 @@ export const authClient = createAuthClient({
   ],
 })
 
-export const getSessionIsomorphic = createIsomorphicFn()
-  .server(() => {
-    const request = getRequest()
-
-    return authClient.getSession({
-      fetchOptions: {
-        headers: {
-          'user-agent': request.headers.get('user-agent') ?? '',
-          'cookie': request.headers.get('cookie') ?? '',
-        },
-      },
-    })
-  })
-  .client(() => authClient.getSession())
+export const getSessionIsomorphic = authClient.getSession
