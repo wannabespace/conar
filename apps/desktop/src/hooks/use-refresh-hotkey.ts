e13f222e6de0dcb22dfda450fb599fc6094d748e
Valerii Strilets <valerii.strilets@gmail.com>
2026-03-29 16:31:49 +0300

    chore: improve useRefreshHotkey

--- apps/desktop/src/hooks/use-refresh-hotkey.ts
+++ apps/desktop/src/hooks/use-refresh-hotkey.ts
@@ -1,12 +1,14 @@
-import { useEffect } from 'react'
+import { useEffect, useEffectEvent } from 'react'
 import { globalHooks } from '~/global-hooks'
 
 export function useRefreshHotkey(refresh: () => unknown, disabled = false) {
+  const refreshEvent = useEffectEvent(refresh)
+
   useEffect(() => {
     return globalHooks.hook('refreshPressed', () => {
       if (!disabled) {
-        refresh()
+        refreshEvent()
       }
     })
-  }, [disabled, refresh])
+  }, [disabled])
 }
