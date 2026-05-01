21fd1769d4a65d57a5f5272897b5fabc76a36fdb
Valerii Strilets <valerii.strilets@gmail.com>
2026-05-02 02:05:20 +0300

    fix: don't fetch subs until sign in

--- apps/app/src/entities/user/hooks/use-subscription.ts
+++ apps/app/src/entities/user/hooks/use-subscription.ts
@@ -1,10 +1,14 @@
 import { ACTIVE_SUBSCRIPTION_STATUSES } from '@conar/shared/constants'
 import { useQuery } from '@tanstack/react-query'
+import { authClient } from '~/lib/auth'
 import { orpc } from '~/lib/orpc'
 import { subscriptionQueryClient } from '~/main'
 
 export function useSubscription() {
-  const { data: list, isPending } = useQuery(orpc.account.subscription.list.queryOptions(), subscriptionQueryClient)
+  const { data } = authClient.useSession()
+  const { data: list, isPending } = useQuery(orpc.account.subscription.list.queryOptions({
+    enabled: !!data?.user.id,
+  }), subscriptionQueryClient)
 
   const subscription = list?.find(s => ACTIVE_SUBSCRIPTION_STATUSES.includes(s.status as typeof ACTIVE_SUBSCRIPTION_STATUSES[number])) ?? null
 
