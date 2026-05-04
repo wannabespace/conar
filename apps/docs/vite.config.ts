734893724b2e5a8475f387faaf14bf394d059159
Valerii Strilets <valerii.strilets@gmail.com>
2026-05-04 14:49:11 +0300

    add base docs

---
+++ apps/docs/vite.config.ts
@@ -0,0 +1,32 @@
+import { PORTS } from '@conar/shared/ports'
+import tailwindcss from '@tailwindcss/vite'
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import react from '@vitejs/plugin-react'
+import mdx from 'fumadocs-mdx/vite'
+import { nitro } from 'nitro/vite'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  server: {
+    port: PORTS.DEV.DOCS,
+  },
+  plugins: [
+    mdx(await import('./source.config')),
+    tailwindcss(),
+    tanstackStart({
+      prerender: {
+        enabled: true,
+      },
+    }),
+    react(),
+    nitro({
+      preset: 'bun',
+    }),
+  ],
+  resolve: {
+    tsconfigPaths: true,
+    alias: {
+      tslib: 'tslib/tslib.es6.js',
+    },
+  },
+})
