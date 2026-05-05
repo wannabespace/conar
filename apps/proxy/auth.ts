5a231d6350514ff568315e3aa0104cd8dbc9e8df
Valerii Strilets <valerii.strilets@gmail.com>
2026-05-05 13:07:00 +0300

    chore: test new proxy headers

--- apps/proxy/auth.ts
+++ apps/proxy/auth.ts
@@ -6,4 +6,13 @@ export const authClient = createAuthClient({
   baseURL: env.API_URL,
   basePath: '/auth',
   plugins: [bearer()],
+  fetchOptions: {
+    onRequest(request) {
+      request.headers.delete('connection')
+      request.headers.delete('content-length')
+      request.headers.delete('host')
+      request.headers.delete('transfer-encoding')
+      return request
+    },
+  },
 })
