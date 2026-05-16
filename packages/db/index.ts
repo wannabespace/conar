05f9a5da27bfaaa1612cea9bc0df9666d96f5a80
Valerii Strilets <valerii.strilets@gmail.com>
2026-05-16 17:17:17 +0300

    minor

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,12 +1,9 @@
-import type { NodePgDatabase } from 'drizzle-orm/node-postgres'
-import process from 'node:process'
 import { drizzle } from 'drizzle-orm/node-postgres'
-import { drizzle as drizzlePglite } from 'drizzle-orm/pglite'
+import { env } from './env'
 import { authRelations } from './schema/auth'
 import { chatsRelations } from './schema/chats'
 import { connectionsRelations } from './schema/connections'
 import { queriesRelations } from './schema/queries'
-import '@electric-sql/pglite'
 
 export const relations = {
   ...authRelations,
@@ -15,10 +12,6 @@ export const relations = {
   ...queriesRelations,
 }
 
-const config = {
+export const db = drizzle(env.DATABASE_URL!, {
   relations,
-} satisfies Parameters<typeof drizzle>[1]
-
-export const db = process.env.NODE_ENV === 'test'
-  ? drizzlePglite(process.env.DATABASE_URL!, config) as unknown as NodePgDatabase<typeof config['relations']>
-  : drizzle(process.env.DATABASE_URL!, config)
+})
