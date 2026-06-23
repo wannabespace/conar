602ebb2ca7854e065613df981b3717f6e3d6c506
Valerii Strilets <valerii.strilets@gmail.com>
2026-06-23 18:33:02 +0300

    updates

--- apps/api/orpc/routers/connections/create.ts
+++ apps/api/orpc/routers/connections/create.ts
@@ -4,7 +4,6 @@ import { SyncType } from '@conar/shared/enums/sync-type'
 import { encrypt } from '@conar/shared/utils/crypto-node'
 import { SafeURL } from '@conar/shared/utils/safe-url'
 import { type } from 'arktype'
-import { sleep } from 'bun'
 import { authMiddleware, orpc } from '~/orpc'
 import { publisher } from './events'
 
@@ -15,8 +14,6 @@ export const create = orpc
   .input(type.or(schema, schema.array()).pipe(data => Array.isArray(data) ? data : [data]))
   .handler(async ({ context, input }) => {
     const userSecret = await context.getUserSecret()
-
-    await sleep(3000)
 
     const inserted = await db.insert(connections).values(await Promise.all(input.map(async (item) => {
       const newConnectionString = new SafeURL(item.connectionString)
