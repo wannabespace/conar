2ad21cdcee18a31c38491ad1b48a2ca08aeaf6b1
Valerii Strilets <valerii.strilets@gmail.com>
2026-07-22 20:48:37 +0300

    updates

--- packages/ui/src/components/tabs.tsx
+++ packages/ui/src/components/tabs.tsx
@@ -46,37 +46,30 @@ function TabsTrigger({ className, ...props }: TabsPrimitive.Tab.Props) {
           justify-center gap-1.5 rounded-lg border border-transparent! px-1.5
           py-0.5 text-sm font-medium whitespace-nowrap text-foreground/60
           transition-all
-          group-data-vertical/tabs:w-full group-data-vertical/tabs:justify-start
-          group-data-vertical/tabs:px-3 group-data-vertical/tabs:py-0.5
-          hover:text-foreground
-          focus-visible:border-ring focus-visible:ring-[3px]
-          focus-visible:ring-ring/50 focus-visible:outline-1
-          focus-visible:outline-ring
-          disabled:pointer-events-none disabled:opacity-50
-          aria-disabled:pointer-events-none aria-disabled:opacity-50
-          [&_svg]:pointer-events-none [&_svg]:shrink-0
-          [&_svg:not([class*='size-'])]:size-4
-        `,
-        `
-          group-data-[variant=line]/tabs-list:bg-transparent
-          data-active:group-data-[variant=line]/tabs-list:border-transparent
-          data-active:group-data-[variant=line]/tabs-list:bg-transparent
-        `,
-        `
-          data-active:border-border! data-active:bg-input
-          data-active:text-foreground data-active:shadow-2xs
-          data-active:group-data-[variant=line]/tabs-list:border-transparent!
-        `,
-        `
-          after:absolute after:bg-foreground after:opacity-0
-          after:transition-opacity
+          group-data-[variant=line]/tabs-list:bg-transparent group-data-vertical/tabs:w-full
+          group-data-vertical/tabs:justify-start group-data-vertical/tabs:px-3
+          group-data-vertical/tabs:py-0.5
+          after:absolute after:bg-foreground
+          after:opacity-0 after:transition-opacity
           group-data-horizontal/tabs:after:inset-x-0
-          group-data-horizontal/tabs:after:-bottom-1.25
-          group-data-horizontal/tabs:after:h-0.5
-          group-data-vertical/tabs:after:inset-y-0
-          group-data-vertical/tabs:after:-right-1
-          group-data-vertical/tabs:after:w-0.5
+          group-data-horizontal/tabs:after:-bottom-1.25 group-data-horizontal/tabs:after:h-0.5
+          group-data-vertical/tabs:after:inset-y-0 group-data-vertical/tabs:after:-right-1
+          group-data-vertical/tabs:after:w-0.5 hover:text-foreground
+          focus-visible:border-ring
+          focus-visible:ring-[3px]
+          focus-visible:ring-ring/50
+          focus-visible:outline-1
+          focus-visible:outline-ring disabled:pointer-events-none
+          disabled:opacity-50 aria-disabled:pointer-events-none
+          aria-disabled:opacity-50
+          data-active:border-border! data-active:bg-input data-active:text-foreground
+          data-active:shadow-2xs
+          data-active:group-data-[variant=line]/tabs-list:border-transparent!
+          data-active:group-data-[variant=line]/tabs-list:bg-transparent
           data-active:group-data-[variant=line]/tabs-list:after:opacity-100
+          [&_svg]:pointer-events-none
+          [&_svg]:shrink-0
+          [&_svg:not([class*='size-'])]:size-4
         `,
         className,
       )}
